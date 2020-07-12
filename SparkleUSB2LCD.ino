64719836ee79acef65599fcf2164dfa00240bc9b
Sparkle <zanjie1999@yeah.net>
2020-07-13 00:34:14 +0800

    add led reverse

--- SparkleUSB2LCD.ino
+++ SparkleUSB2LCD.ino
@@ -6,13 +6,33 @@
 #define GPIO_ADDR     0x27             // (PCA8574A A0-A2 @5V) typ. A0-A3 Gnd 0x20 / 0x38 for A - 0x27 is the address of the Digispark LCD modules.
 
 LiquidCrystal_I2C lcd(GPIO_ADDR, 16, 2); // set address & 16 chars / 2 lines
+boolean backlight = 0;
+boolean reverseBacklight = 1;
 
+void led(int isOn) {
+  if (isOn) {
+    if (reverseBacklight) {
+      lcd.noBacklight();
+    } else {
+      lcd.backlight();
+    }
+    backlight = 1;
+  } else {
+    if (reverseBacklight) {
+      lcd.backlight();
+    } else {
+      lcd.noBacklight();
+    }
+    backlight = 0;
+    
+  }
+}
 
 void setup() {
   DigiUSB.begin();
   TinyWireM.begin();                    // initialize I2C lib - comment this out to use with standard arduinos
   lcd.init();                           // initialize the lcd
-  //  lcd.backlight();                      // Print a message to the LCD.
+  led(0);
   lcd.print("Sparkle  USB2LCD");
   lcd.setCursor(0, 1);
   lcd.print("    cupinkie.com");
@@ -24,7 +44,6 @@ void loop() {
   int currentLine = 0;
   boolean clearOnNext = 0;
   boolean clearMsg = 1;
-  boolean backlight = 1;
   // when there are no characters to read, or the character isn't a newline
   int lastRead;
   int lastRead2;
@@ -61,14 +80,7 @@ void loop() {
         lcd.setCursor(0, currentLine);
         num = 0;
       } else if (lastRead == 172) { //not sign "¬" send it with the send program to toggle the backlight
-        if (backlight) {
-          lcd.noBacklight();
-          backlight = 0;
-        }
-        else {
-          lcd.backlight();
-          backlight = 1;
-        }
+        led(!backlight);
         DigiUSB.read(); //read to nothing to get rid of newline that should come after it
       } else {
         if (clearOnNext) {
